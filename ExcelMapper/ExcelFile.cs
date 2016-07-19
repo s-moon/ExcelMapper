df8f047c16e11c01c64b8896573074b609c6ea13
Stephen Moon <stephen@logicalmoon.com>
2016-07-19 21:37:02 +0100

    First commit.

---
+++ ExcelMapper/ExcelFile.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using Microsoft.Office.Interop.Excel;
+
+namespace ExcelMapper
+{
+    public class ExcelFile : IDisposable
+    {
+        private Application excel;
+        private Workbook workbook;
+        private Worksheet worksheet;
+        private string filename;
+        public ExcelFile(string filename)
+        {
+            this.filename = filename;
+            var excel = new Application();
+            if (excel == null)
+            {
+                Console.WriteLine("oops - no excel");
+                Environment.Exit(1);
+            }
+            workbook = excel.Workbooks.Open(filename, true, true);
+            var cellk = (Microsoft.Office.Interop.Excel.Worksheet)workbook.Worksheets;
+        }
+
+        public void Process()
+        {
+            worksheet = (Worksheet)workbook.Worksheets.get_Item(1);
+            var cellValue = worksheet.Cells[1, 1];
+        }
+
+        public void Close()
+        {
+            if (workbook != null)
+            {
+                workbook.Close(false);
+            }
+            
+            if (excel != null)
+            {
+                excel.Quit();
+            }
+        }
+
+        public void Dispose()
+        {
+            if (worksheet != null)
+            {
+                releaseObject(worksheet);
+            }
+
+            if (workbook != null)
+            {
+                releaseObject(workbook);
+            }
+            
+            if (excel != null)
+            {
+                releaseObject(excel);
+            }
+        }
+
+        private void releaseObject(object obj)
+        {
+            try
+            {
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(obj);
+                obj = null;
+            }
+            catch (Exception ex)
+            {
+                obj = null;
+                Console.WriteLine("oops - cleaning up");
+            }
+            finally
+            {
+                GC.Collect();
+            }
+        }
+    }
+}
---
+++ ExcelMapper/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.IO;
+
+using System.Xml.Serialization;
+using System;
+
+namespace ExcelMapper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XmlSerializer deserializer = new XmlSerializer(typeof(ExcelMap));
+            TextReader reader = new StreamReader(@"ExcelMapping.xml");
+            var excelMapData = (ExcelMap)deserializer.Deserialize(reader);
+            using (var xf = new ExcelFile(@"F:\Projects\Open Source\ExcelMapper\ExcelMapper\bin\Debug\excel.xlsx"))
+            {
+                xf.Process();
+                xf.Close();
+            }
+            reader.Close();
+        }
+        private static void releaseObject(object obj)
+        {
+            try
+            {
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(obj);
+                obj = null;
+            }
+            catch (Exception ex)
+            {
+                obj = null;
+                Console.WriteLine("oops - cleaning up");
+            }
+            finally
+            {
+                GC.Collect();
+            }
+        }
+    }
+}
