ebb9f41a8fe99a818718dc544fd71dd5f7ec914b
Stephen Moon <stephen@logicalmoon.com>
2016-07-26 12:54:00 +0100

    Exception handling for file finding.

--- ExcelMapper/FileMonitor.cs
+++ ExcelMapper/FileMonitor.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using NLog;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Security;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,6 +11,8 @@ namespace ExcelMapper
 {
     public class FileMonitor
     {
+        private Logger logger = LogManager.GetCurrentClassLogger();
+
         public FileMonitor(string folder, string fileMask)
         {
             Folder = folder;
@@ -21,13 +25,37 @@ namespace ExcelMapper
 
         public string[] AvailableFiles(DateTime newerThanDate)
         {
-            DirectoryInfo d = new DirectoryInfo(Folder);
-            FileInfo[] files = d.GetFiles(FileMask); 
+            string[] results = null;
+            try
+            {
+                DirectoryInfo d = new DirectoryInfo(Folder);
+                FileInfo[] files = d.GetFiles(FileMask);
 
-            var results = files
-                .Where(i => i.LastWriteTime >= newerThanDate)
-                .Select(i => i.FullName).ToArray();
-            
+                results = files
+                    .Where(i => i.LastWriteTime >= newerThanDate)
+                    .Select(i => i.FullName).ToArray();
+            }
+            catch (ArgumentNullException e)
+            {
+                logger.Error("Is the file or folder name missing? " + e);
+                Environment.Exit(-1);
+            }
+            catch (DirectoryNotFoundException e)
+            {
+                logger.Error("Unable to access folder: " + Folder + " : "  + e);
+                Environment.Exit(-1);
+            }
+            catch (SecurityException e)
+            {
+                logger.Error("Oops. This looks like a permissions problem with folder: " + Folder + " : " + e);
+                Environment.Exit(-1);
+            }
+            catch(Exception e)
+            {
+                logger.Error("Something terrible happened that I couldn't work around. " + e);
+                Environment.Exit(-1);
+            }
+           
             return results;
         }
     }
